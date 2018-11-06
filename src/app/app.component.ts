d392172c5d1b129ce9abd640a60e0ac707312505
Martin <mar.gardemann@gmail.com>
2018-11-06 07:09:38 +0100

    Adds header component and scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'test-app';
+
+  clickHandler(){
+    alert('I AM CLICKED');
+  }
 }
