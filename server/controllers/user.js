a855d2dcaf15e28768a036dfccd2f37534c696b9
Martin <mar.gardemann@gmail.com>
2018-11-16 19:04:59 +0100

    adds register, adds login, auth middleware and routs on server

---
+++ server/controllers/user.js
@@ -0,0 +1,124 @@
+const User = require ('../models/user');
+const {normalizeErrors} = require ('../helpers/mongoose');
+const jwt = require ('jsonwebtoken');
+const config = require('../config/dev');
+
+exports.auth = function (req,res) {
+  const {email, password} =req.body;
+
+  if(!password||!email) {
+    return  res.status(422).send({
+       errors:[{
+         title:'Data missing!',
+         details:'Provide email and password'
+       }]});
+   }
+
+   User.findOne({email},function (err,user){
+    if (err){
+      return res.status(422).send({errors:normalizeErrors(err.errors)});
+    }
+
+    if(!user) {
+      return  res.status(422).send({
+        errors:[{
+          title:'Invalid user',
+          details:'User does not exist'
+        }]});
+    }
+    if(user.isSamePassword(password)){
+      const token= jwt.sign({
+        userId: user.id,
+        username:user.username
+      }, config.SECRET, { expiresIn: '1h' });
+
+      return res.json(token);
+
+    } else {
+      return  res.status(422).send({
+        errors:[{
+          title:'Wrong data',
+          details:'Wrong email or password'
+        }]});
+    }
+
+   });
+}
+
+
+
+exports.register = function (req,res) {
+  const {username,email, password,passwordConfirmation} =req.body;
+  
+
+  if(!password||!email) {
+   return  res.status(422).send({
+      errors:[{
+        title:'Data missing!',
+        details:'Provide email and password'
+      }]});
+  }
+
+  if(password !== passwordConfirmation) {
+    return res.status(422).send({
+      errors:[{
+        title:'Invalid password!',
+        details:'Password is not the same as confirmation!'
+      }]});
+  }
+
+  User.findOne({email},function(err,existingUser){
+    if (err){
+      return res.status(422).send({errors:normalizeErrors(err.errors)});
+    }
+
+    if (existingUser) {
+      return res.status(422).send({errors:[{titel:'invalid email',detail :'user with this mail already exists'}]});
+    }
+
+    const user = new User({
+      username,
+      email,
+      password
+    });
+    
+    user.save(function(err){
+      if (err){
+        return res.status(422).send({errors:normalizeErrors(err.errors)});
+      }
+      return res.status(200).json({'registered':true});
+    });
+  });
+
+}
+
+exports.authMiddleware = function (req,res,next) {
+  const token = req.headers.authorization;
+  if (token) {
+    const user = parseToken (token);
+    User.findById(user.userId, function (err,user) {
+      if (err) {
+        return res.status(422).send({errors:normalizeErrors(err.errors)});
+      }
+
+      if (user) {
+        res.locals.user = user;
+        next();
+      } else {
+        notAuthorized();
+      }
+
+    })
+  } else {
+    notAuthorized(res);
+}
+
+}
+
+function parseToken(token) {
+  return jwt.verify(token.split(' ')[1], config.SECRET);
+}
+
+function notAuthorized(res){
+  return res.status(401).send({errors:[{titel:'not authorized',detail :'Please login'}]});
+}
--- server/fake-db.js
+++ server/fake-db.js
@@ -1,4 +1,5 @@
-const Rental = require('./rental');
+const Rental = require('./models/rental');
+const User = require ('./models/user');
 
 class FakeDb{
   constructor(){
@@ -34,24 +35,35 @@ class FakeDb{
         shared: true,
         description: "Very nice apartment in center of the city.",
         dailyRate: 23
-     }]
+     }];
+
+    this.users =[{
+      username:"Test User",
+      email:"test@gmail.com",
+      password:"testtest"
+    }];
   }
 
   async cleandDb (){
     await Rental.deleteMany();
+    await User.deleteMany();
   }
 
-  pushRentalsToDb() {
+  pushDataToDb() {
+    const user = new User(this.users[0]);
+
     this.rentals.forEach((rental) =>{
         const newRental = new Rental(rental);
+        newRental.user = user;
+        user.rentals.push(newRental);
         newRental.save();
-
-    })
+    });
+    user.save();
   }
 
-  seedDb(){
-    this.cleandDb();
-    this.pushRentalsToDb();
+  async seedDb(){
+    await this.cleandDb();
+    this.pushDataToDb();
   }
 }
 
